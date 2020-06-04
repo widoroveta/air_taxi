0dfacbc10d595a388f96048b8d34921adf971699
widoroveta <55321418+widoroveta@users.noreply.github.com>
2020-06-04 07:21:36 -0300

    version 1.00

---
+++ src/com/company/Person.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int DNI;
+    private int age;
+    public Person (){}
+    public Person(String name,String surname,int DNI,int age)
+    {
+        this.DNI=DNI;
+        this.name=name;
+        this.surname=surname;
+        this.age=age;
+    }
+}
