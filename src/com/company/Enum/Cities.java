21eed134c2850081f03eaf2b3524720884fb487f
widoroveta <55321418+widoroveta@users.noreply.github.com>
2020-06-07 20:28:08 -0300

    agregar enum

---
+++ src/com/company/Enum/Cities.java
@@ -0,0 +1,15 @@
+package com.company.Enum;
+
+public enum Cities {
+    BsAs("Buenos Aires"),
+    CBA("Cordoba"),
+    SCL("Santiago De Chile"),
+    MVD("Montevideo");
+
+
+    private String cities;
+
+    Cities(String cities) {
+        this.cities = cities;
+    }
+    }
