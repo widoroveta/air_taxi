e669d7d63d14342610f4db80bc5b269d53baa515
widoroveta <55321418+widoroveta@users.noreply.github.com>
2020-06-08 15:31:37 -0300

    version 1.03

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -5,12 +5,43 @@ public class Person {
     private String surname;
     private int DNI;
     private int age;
-    public Person (){}
-    public Person(String name,String surname,int DNI,int age)
-    {
-        this.DNI=DNI;
-        this.name=name;
-        this.surname=surname;
-        this.age=age;
+
+    public Person() {
+    }
+
+    public Person(String name, String surname, int DNI, int age) {
+        this.DNI = DNI;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public boolean checkIn(String name, String Surname, int DNI, int age) {
+        boolean check = true;
+        while (check) {
+            if (this.name.equalsIgnoreCase(name)) {
+                check = false;
+            }
+            if (this.surname.equalsIgnoreCase(surname)) {
+                check = false;
+            }
+            if (this.age != age) {
+                check = false;
+            }
+            if (this.DNI!=DNI)
+            {
+                check=false;
+            }
+        }
+
+        return check;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\n' +
+                ", surname='" + surname + '\n' +
+                ", DNI=" + DNI + '\n' +
+                ", age=" + age + '\n';
     }
 }
